fefeaa1426864d4b1ddf30219f5c3e5bbeac73de
Ikaros <liukun@onesight.com>
2020-01-14 20:05:21 +0800

    test(api): :white_check_mark: add tests for get captions

--- pyyoutube/api.py
+++ pyyoutube/api.py
@@ -1805,6 +1805,7 @@ class Api(object):
         *,
         video_id: str,
         parts: Optional[Union[str, list, tuple, set]] = None,
+        caption_id: Optional[Union[str, list, tuple, set]] = None,
         return_json: bool = False,
     ):
         """
@@ -1820,6 +1821,9 @@ class Api(object):
                 The resource parts for caption you want to retrieve.
                 If not provide, use default public parts.
                 You can pass this with single part str, comma-separated parts str or a list,tuple,set of parts.
+            caption_id ((str,list,tuple,set)):
+                The id for caption that you want to get data.
+                You can pass this with single id str,comma-separated id str, or list, tuple, set of id str.
             return_json(bool, optional):
                 The return data type. If you set True JSON data will be returned.
                 False will return a pyyoutube.CaptionListResponse instance.
@@ -1832,46 +1836,10 @@ class Api(object):
             "part": enf_parts("captions", parts),
         }
 
-        resp = self._request(resource="captions", method="GET", args=args)
-        data = self._parse_response(resp)
-
-        if return_json:
-            return data
-        else:
-            return CaptionListResponse.from_dict(data)
-
-    def get_captions_by_id(
-        self,
-        *,
-        caption_id: Union[str, list, tuple, set],
-        parts: Optional[Union[str, list, tuple, set]] = None,
-        return_json: Optional[bool] = False,
-    ):
-        """
-        Retrieve caption data by given caption id.
-
-        Args:
-            caption_id ((str,list,tuple,set)):
-                The id for caption that you want to get data.
-                You can pass this with single id str,comma-separated id str, or list, tuple, set of id str.
-            parts ((str,list,tuple,set) optional):
-                The resource parts for caption you want to retrieve.
-                If not provide, use default public parts.
-                You can pass this with single part str, comma-separated parts str or a list,tuple,set of parts.
-            return_json(bool, optional):
-                The return data type. If you set True JSON data will be returned.
-                False will return a pyyoutube.CaptionListResponse instance.
-        Returns:
-            CaptionListResponse or original data.
-        """
-
-        args = {
-            "id": enf_comma_separated("caption_id", caption_id),
-            "part": enf_parts("captions", parts),
-        }
+        if caption_id is not None:
+            args["id"] = enf_comma_separated("caption_id", caption_id)
 
         resp = self._request(resource="captions", method="GET", args=args)
-
         data = self._parse_response(resp)
 
         if return_json:
