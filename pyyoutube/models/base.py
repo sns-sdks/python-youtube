eb9ae1833d1eb80a939873b53f91fb1cdcf45fa3
ikaroskun <merle.liukun@gmail.com>
2021-05-14 15:55:40 +0800

    feat(model): :sparkles: update base model to save original data

--- pyyoutube/models/base.py
+++ pyyoutube/models/base.py
@@ -1,10 +1,18 @@
 from dataclasses import dataclass
+from typing import Type, TypeVar
 
 from dataclasses_json import DataClassJsonMixin
+from dataclasses_json.core import Json, _decode_dataclass
+
+A = TypeVar("A", bound="DataClassJsonMixin")
 
 
 @dataclass
 class BaseModel(DataClassJsonMixin):
     """Base model class for instance use."""
 
-    ...
+    @classmethod
+    def from_dict(cls: Type[A], kvs: Json, *, infer_missing=False) -> A:
+        # save original data for lookup
+        cls._json = kvs
+        return _decode_dataclass(cls, kvs, infer_missing)
