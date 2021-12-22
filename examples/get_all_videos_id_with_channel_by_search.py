dce1a8dbbf3eb7334f114e7d79f33c7b0cbad9a6
ikaroskun <merle.liukun@gmail.com>
2021-12-22 19:08:00 +0800

    feat(examples): :memo: update example code

--- examples/get_all_videos_id_with_channel_by_search.py
+++ examples/get_all_videos_id_with_channel_by_search.py
@@ -1,9 +1,7 @@
 """
-Retrieve some videos info from given channel.
+Retrieve channel's videos by search api.
 
-Use pyyoutube.api.get_channel_info to get channel video uploads playlist id.
-Then use pyyoutube.api.get_playlist_items to get playlist's videos id.
-Last use get_video_by_id to get videos data.
+Note Quota impact: A call to this method has a quota cost of 100 units.
 """
 
 import pyyoutube
@@ -14,14 +12,14 @@ API_KEY = "xxx"  # replace this with your api key.
 def get_all_videos_id_by_channel(channel_id, limit=50, count=50):
     api = pyyoutube.Api(api_key=API_KEY)
     res = api.search(channel_id=channel_id, limit=limit, count=count)
-    nextPage = res.nextPageToken
-    videos_id = []
+    next_page = res.nextPageToken
+    videos = []
 
-    while nextPage:
-        nextPage = res.nextPageToken
+    while next_page:
+        next_page = res.nextPageToken
         for item in res.items:
             if item.id.videoId:
-                videos_id.append(item.id.videoId)
+                videos.append(item.id.videoId)
 
         res = api.search(
             channel_id=channel_id,
@@ -30,4 +28,4 @@ def get_all_videos_id_by_channel(channel_id, limit=50, count=50):
             page_token=res.nextPageToken,
         )
 
-    return videos_id
+    return videos
