530363bfe4125039f5b9e320a26601bd53d6426b
ikaroskun <merle.liukun@gmail.com>
2022-10-20 17:34:09 +0800

    feat(client): :construction: new structure for client impl

--- pyyoutube/__init__.py
+++ pyyoutube/__init__.py
@@ -1,4 +1,5 @@
 from .api import Api  # noqa
+from .client import Client  # noqa
 from .error import *  # noqa
 from .models import *  # noqa
 from .utils.constants import TOPICS  # noqa
---
+++ pyyoutube/resources/__init__.py
@@ -0,0 +1,1 @@
+from .channel import Channel  # noqa
