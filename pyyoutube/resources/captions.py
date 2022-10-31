f1bc93e733ea79ce22bd94a213c3e14a917ca01f
ikaros <merle.liukun@gmail.com>
2022-10-31 23:06:12 +0800

    WIP(caption): :construction: how to upload file?

--- pyyoutube/resources/captions.py
+++ pyyoutube/resources/captions.py
@@ -4,7 +4,7 @@
 from typing import Optional, Union
 
 from pyyoutube.resources.base_resource import Resource
-from pyyoutube.models import CaptionListResponse
+from pyyoutube.models import Caption, CaptionListResponse
 from pyyoutube.utils.params_checker import enf_comma_separated, enf_parts
 
 
@@ -55,5 +55,55 @@ class CaptionsResource(Resource):
             **kwargs,
         }
         response = self._client.request(path="captions", params=params)
+        data = self._client.parse_response(response=response)
+        return data if return_json else CaptionListResponse.from_dict(data)
+
+    def insert(
+        self,
+        part: str,
+        body: Union[dict, Caption],
+        on_behalf_of_content_owner: Optional[str] = None,
+        sync: Optional[bool] = None,
+        return_json: bool = False,
+        **kwargs,
+    ) -> Union[dict, CaptionListResponse]:
+        """Uploads a caption track.
+
+        Args:
+            part:
+                The part parameter specifies the caption resource parts that
+                the API response will include. Set the parameter value to snippet.
+            body:
+                Provide caption data in the request body. You can give dataclass or just a dict with data.
+            on_behalf_of_content_owner:
+                This parameter can only be used in a properly authorized request.
+                Note: This parameter is intended exclusively for YouTube content partners.
+            sync:
+                The sync parameter indicates whether YouTube should automatically synchronize the caption
+                file with the audio track of the video.
+                If you set the value to true, YouTube will disregard any time codes that are in the uploaded
+                caption file and generate new time codes for the captions.
+            return_json:
+                Type for returned data. If you set True JSON data will be returned.
+            **kwargs:
+                Additional parameters for system parameters.
+                Refer: https://cloud.google.com/apis/docs/system-parameters.
+
+        Returns:
+            Caption data.
+        """
+
+        params = {
+            "part": part,
+            "onBehalfOfContentOwner": on_behalf_of_content_owner,
+            "sync": sync,
+            **kwargs,
+        }
+        response = self._client.request(
+            method="POST",
+            path="captions",
+            params=params,
+            json=body,
+        )
         data = self._client.parse_response(response=response)
         return data if return_json else CaptionListResponse.from_dict(data)
