e0c872b5c3537c73d5c6e662616e49fbe06112a9
ikaroskun <merle.liukun@gmail.com>
2022-11-03 19:21:38 +0800

    WIP(caption): :construction: work on upload file

--- pyyoutube/resources/captions.py
+++ pyyoutube/resources/captions.py
@@ -20,7 +20,7 @@ class CaptionsResource(Resource):
         self,
         parts: Optional[Union[str, list, tuple, set]] = None,
         video_id: Optional[str] = None,
-        id: Optional[Union[str, list, tuple, set]] = None,
+        caption_id: Optional[Union[str, list, tuple, set]] = None,
         on_behalf_of_content_owner: Optional[str] = None,
         return_json: bool = False,
         **kwargs: Optional[dict],
@@ -33,7 +33,7 @@ class CaptionsResource(Resource):
             video_id:
                 The parameter specifies the YouTube video ID of the video for which the API
                 should return caption tracks.
-            id:
+            caption_id:
                 The id parameter specifies a comma-separated list of IDs that identify the
                 caption resources that should be retrieved.
             on_behalf_of_content_owner:
@@ -50,9 +50,9 @@ class CaptionsResource(Resource):
         """
 
         params = {
-            "part": enf_parts(resource="", value=parts),
+            "part": enf_parts(resource="captions", value=parts),
             "videoId": video_id,
-            "id": enf_comma_separated(field="id", value=id),
+            "id": enf_comma_separated(field="caption_id", value=caption_id),
             "onBehalfOfContentOwner": on_behalf_of_content_owner,
             **kwargs,
         }
@@ -103,7 +103,56 @@ class CaptionsResource(Resource):
         }
         response = self._client.request(
             method="POST",
-            path="captions",
+            path="",
+            params=params,
+            json=body,
+        )
+        data = self._client.parse_response(response=response)
+        return data if return_json else CaptionListResponse.from_dict(data)
+
+    def update(
+        self,
+        part: str,
+        body: Union[dict, Caption],
+        on_behalf_of_content_owner: Optional[str] = None,
+        sync: Optional[bool] = None,
+        return_json: bool = False,
+        **kwargs,
+    ) -> Union[dict, CaptionListResponse]:
+        """Updates a caption track.
+
+        Args:
+            part:
+                The part parameter specifies the caption resource parts that
+                the API response will include. Set the parameter value to snippet.
+            body:
+                Provide caption data in the request body. You can give dataclass or just a dict with data.
+            on_behalf_of_content_owner:
+                This parameter can only be used in a properly authorized request.
+                Note: This parameter is intended exclusively for YouTube content partners.
+            sync:
+                The sync parameter indicates whether YouTube should automatically synchronize the caption
+                file with the audio track of the video.
+                If you set the value to true, YouTube will disregard any time codes that are in the uploaded
+                caption file and generate new time codes for the captions.
+            return_json:
+                Type for returned data. If you set True JSON data will be returned.
+            **kwargs:
+                Additional parameters for system parameters.
+                Refer: https://cloud.google.com/apis/docs/system-parameters.
+        Returns:
+            Caption data.
+
+        """
+        params = {
+            "part": part,
+            "onBehalfOfContentOwner": on_behalf_of_content_owner,
+            "sync": sync,
+            **kwargs,
+        }
+        response = self._client.request(
+            method="PUT",
+            path="",
             params=params,
             json=body,
         )
