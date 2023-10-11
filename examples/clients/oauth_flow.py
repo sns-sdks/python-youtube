f4cfee5ab4ff86b07230347b6292a6bef8ffa5b5
pidi3000 <pidi3000@live.com>
2023-10-12 00:19:26 +0200

    Added example for client_secret.json

--- examples/clients/oauth_flow.py
+++ examples/clients/oauth_flow.py
@@ -4,8 +4,11 @@
 
 from pyyoutube import Client
 
+
 CLIENT_ID = "xxx"  # Your app id
 CLIENT_SECRET = "xxx"  # Your app secret
+CLIENT_SECRET_PATH = None  # or your path/to/client_secret_web.json
+
 SCOPE = [
     "https://www.googleapis.com/auth/youtube",
     "https://www.googleapis.com/auth/youtube.force-ssl",
@@ -15,6 +18,8 @@ SCOPE = [
 
 def do_authorize():
     cli = Client(client_id=CLIENT_ID, client_secret=CLIENT_SECRET)
+    # or if you want to use a web type client_secret.json
+    # cli = Client(client_secret_path=CLIENT_SECRET_PATH)
 
     authorize_url, state = cli.get_authorize_url(scope=SCOPE)
     print(f"Click url to do authorize: {authorize_url}")
